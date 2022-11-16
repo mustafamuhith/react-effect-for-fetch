ab86873cd4876a72efccb5fb44b38908b40923d7
Mustafa <mustafamuhith@gmail.com>
2022-11-16 16:22:26 +0000

    barely started

---
+++ src/sections/Advice/components/AdviceSlip.js
@@ -0,0 +1,16 @@
+import { useState } from "react"
+function AdviceSlip () {
+return (
+    
+ <>
+  <h3>Some Advice</h3>
+  <p>Always the burrito.</p>
+  <button>Get More Advice</button>
+  <button>Save to Favourties</button>
+
+</>
+
+)
+}
+
+export default AdviceSlip
--- src/sections/Advice/index.js
+++ src/sections/Advice/index.js
@@ -1,11 +1,33 @@
 import { useEffect, useState } from "react"
 
 function AdviceSection() {
+
+  const [advice, setAdvice] = useState('')
+
+  console.log({advice})
+
+  useEffect(() => {
+
+    fetch('')
+  })
+
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="adivce-slip"><section class="adivce-slip">
+    <h3>Some Advice</h3>
+    <p>Always the burrito.</p>
+    <button>Get More Advice</button>
+    <button>Save to Favourties</button>
+  </section></section>
+      <section className="favourtite-slips-list"><section class="favourtite-slips-list">
+    <h3>Favourite Advice Slips</h3>
+    <ul>
+      <li>Measure twice, cut once.</li>
+      <li>Don't let the bastards grind you down.</li>
+      <li>Always the burrito.</li>
+    </ul>
+  </section></section>
     </section>
   )
 }
